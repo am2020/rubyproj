0d4a4909a1bfaa277d75ad7c7c34f96d04d7d0ab
Abdi Mohamud <amohamud92@icloud.com>
2017-06-08 11:28:23 +0100

    Added chrome web driver to spec helper

--- Integration/spec/spec_helper.rb
+++ Integration/spec/spec_helper.rb
@@ -1,1 +1,16 @@
 require 'rspec'
+require 'selenium-webdriver'
+
+RSpec.configure do |config|
+  config.around(:example) do |example|
+  	# Initialize chrome webapp
+    @driver = Selenium::WebDriver.for :chrome 
+    begin
+      # Start chrome 
+      @driver.run
+    ensure
+      # End chrome
+      @driver.quit
+    end
+  end
+end
